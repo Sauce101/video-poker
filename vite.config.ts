60ccc73c94dc200a5e53f3939681f18f4b53ff1a
Sauce101 <42197557+Sauce101@users.noreply.github.com>
2023-02-07 01:50:41 -0800

    Added vite-plugin-pwa and icons plus manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,59 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Video Poker',
+        short_name: 'Video Poker',
+        start_url: '/',
+        display: 'standalone',
+        orientation: 'landscape',
+        background_color: '#000000',
+        lang: 'en',
+        scope: '/',
+        icons: [
+          {
+            src: 'maskable',
+            sizes: '1024x1024',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/logo192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/logo256.png',
+            sizes: '256x256',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/logo384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+          {
+            src: '/logo512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#000000',
+      },
+    }),
+  ],
+});
